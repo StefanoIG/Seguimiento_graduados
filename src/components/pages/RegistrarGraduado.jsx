02d00d4521faf4a5d04bab4d124037c554658936
StefanoIG <stefano20033@gmail.com>
2023-12-07 05:43:24 -0500

    v2

--- src/components/pages/RegistrarGraduado.jsx
+++ src/components/pages/RegistrarGraduado.jsx
@@ -131,41 +131,52 @@ const RegistrarGraduado = () => {
         </ul>
       </header>
       <div className="content">
-        <h1>Registrar Graduado</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="">Cedula</label>
-            <input type="text" id="cedula" name="cedula" value={formData.cedula} onChange={handleChange} required/>
-          </div>
-          <div>
-            <label htmlFor="">Nombre</label>
-            <input type="text" id="nombre" name="name"  value={formData.name} onChange={handleChange} required/>
-          </div>
-          <div>
-            <label htmlFor="">Apellidos</label>
-            <input type="text" id="apellidos" name="lastname" value={formData.lastname} onChange={handleChange} required/>
-          </div>
-          <div>
-            <label htmlFor="">Direccion</label>
-            <input type="text" id="direccion" name="direccion" value={formData.direccion} onChange={handleChange} required/>
-          </div>
-          <div>
-            <label htmlFor="">Telefono</label>
-            <input type="text" id="telefono" name="telefono" value={formData.telefono} onChange={handleChange} required/>
-          </div>
-          <div>
-            <label htmlFor="">Correo</label>
-            <input type="text" id="correo" name="email" value={formData.email} onChange={handleChange} required/>
-          </div>
-          <div>
-            <label htmlFor="">Fecha de graduo</label>
-            <input type="date" id="date" name="date" />
-          </div>
-          <div>
-            <button type="submit">Registrar</button>
-          </div>
-        </form>
-      </div>
+          <h1 className="titulo">Registrar Graduado</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="campo">
+              <label htmlFor="cedula">Cedula</label>
+              <input type="number" id="cedula" name="cedula" value={formData.cedula} onChange={handleChange} required />
+            </div>
+            <div className="campo">
+              <label htmlFor="nombre">Nombre</label>
+              <input type="text" id="nombre" name="name" value={formData.name} onChange={handleChange} required />
+            </div>
+            <div className="campo">
+              <label htmlFor="apellidos">Apellidos</label>
+              <input type="text" id="apellidos" name="lastname" value={formData.lastname} onChange={handleChange} required />
+            </div>
+            <div className="campo">
+              <label htmlFor="direccion">Direccion</label>
+              <input type="text" id="direccion" name="direccion" value={formData.direccion} onChange={handleChange} required />
+            </div>
+            <div className="campo">
+              <label htmlFor="telefono">Telefono</label>
+              <input type="text" id="telefono" name="telefono" value={formData.telefono} onChange={handleChange} required />
+            </div>
+            <div className="campo">
+              <label htmlFor="correo">Correo</label>
+              <input type="text" id="correo" name="email" value={formData.email} onChange={handleChange} required />
+            </div>
+            <div className="campo">
+              <label htmlFor="date">Fecha de graduación</label>
+              <input type="date" id="date" name="date" />
+            </div>
+            <div className="campo">
+              <label htmlFor="carrera">Carrera de egreso</label>
+              <input type="text" id="carrera" name="carrera" />
+            </div>
+            <div className="campo">
+              <label htmlFor="chamba">¿Está trabajando en algo relacionado a su carrera?</label>
+              <input type="text" id="chamba" name="chamba" />
+            </div>
+
+            <div className="boton-container">
+              <button className="boton" type="submit">Registrar</button>
+            </div>
+          </form>
+        </div>
+
+
     </div>
   )
 }
